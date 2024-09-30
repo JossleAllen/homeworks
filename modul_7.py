1bbc7f3c8cbd9405ff9eff4573bf776cc79d0ccb
user <kirova1825@gmail.com>
2024-09-30 20:15:43 +0800

    home8

---
+++ modul_7.py
@@ -0,0 +1,16 @@
+my_dict={"Alena": 8945687342674, "Sasha": 67835460912768}
+print(my_dict)
+my_dict["Alena"] = 8945687342674
+my_dict["Yana"] = 39567239645
+print(my_dict)
+del my_dict["Sasha"]
+print(my_dict)
+
+my_set={1,2,3,4,5,1,2,3,4,5, 'Strind', True,(1,2,3,4)}
+print(my_set)
+my_set={2,3,4,6,8}
+print(my_set)
+print(my_set.discard(8))
+print(my_set)
+
+
