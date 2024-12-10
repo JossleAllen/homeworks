5046f0e1cf4ef1803cca78f008848994662c1e89
JossleAllen <kirova1825@gmail.com>
2024-12-10 23:08:30 +0800

    Add files via upload

--- modul_2_hard_.py
+++ modul_2_hard_.py
@@ -1,8 +1,8 @@
 n = int(input('Введите целое число от 3 до 20: '))
 def get_password(number):
     password = ''
-    for i in range(1, number):
-        for j in range(2, number):
+    for i in range( 1, number):
+        for j in range( i + 1, number):
             if j <= i:
                 continue
             if number % (i + j) == 0:
