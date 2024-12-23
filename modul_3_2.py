6b53644823178fc78c2f3397e8bc72e4b25966dd
JossleAllen <kirova1825@gmail.com>
2024-12-23 20:30:42 +0800

    Add files via upload

--- modul_3_2.py
+++ modul_3_2.py
@@ -1,22 +1,20 @@
 def send_email(massage, recipient, *, sender="university.help@gmail.com"):
-    a = "@"
     while 1 > 0:
         if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
-            if a not in recipient and sender:
+            if  not  recipient and sender:
                 print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-                break
         else:
             print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-            break
         if sender == recipient:
             print("Нельзя отправить письмо самому себе!")
-            break
+            return
         elif sender == "university.help@gmail.com":
             print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
-            break
         elif sender != "university.help@gmail.com":
             print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
-            break
+        return
+
+
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
